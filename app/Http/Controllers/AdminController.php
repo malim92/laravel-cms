d7c7462aa3929fbb5e11450287429026be8aacb7
Ali <malim92@live.com>
2023-12-14 12:43:44 +0200

    setup laravel, vue, admin-lte,sanctum

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AdminController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('token-name')->plainTextToken;
+
+            return response()->json(['token' => $token, 'user' => $user], 200);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
---
+++ resources/js/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <form @submit.prevent="login">
+            <label>Email:</label>
+            <input v-model="email" type="email" required />
+
+            <label>Pass:</label>
+            <input v-model="password" type="password" required />
+
+            <button type="submit">Login </button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        login() {
+            // Implement the logic to make a login API request
+            // using the email and password from the data.
+            // You can use Axios or another HTTP library for this.
+        },
+    },
+};
+</script>
